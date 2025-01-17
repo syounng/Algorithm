4cdd01dc3eee31ad76c4c931c6573fe21ad1530e
syounng <stellaj0522@gmail.com>
2025-01-18 02:00:06 +0900

    [Silver I] Title: 트럭, Time: 120 ms, Memory: 15288 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -6,35 +6,45 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int w = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		Queue<Integer> truck = new LinkedList<Integer>();
-		st = new StringTokenizer(br.readLine());
-		for(int i= 0; i< n; i++) {
-			truck.offer(Integer.parseInt(st.nextToken()));
-		}
-		int time = 0; 
-		int bw=0;
-		Queue<Integer> q = new LinkedList<Integer>();
-		for(int i =0; i<w ; i++) {
-			q.add(0);
-		}
-		while(!q.isEmpty()) {
-			time++;
-			bw-=q.poll();
-			if(!truck.isEmpty()) {
-				if(truck.peek()+bw<=L) {
-					bw+=truck.peek();
-					q.offer(truck.poll());
-				}else {
-					q.offer(0);
-				}
-			}
-		}
-		System.out.println(time);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        //값 입력받기
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        //trucks를 큐로 생성하고 모든 트럭을 넣음
+        Queue<Integer> trucks = new LinkedList<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            trucks.offer(Integer.parseInt(st.nextToken()));
+        }
+
+        int time = 0;
+        int nowWeight = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+
+        //다리를 0으로 채워서 초기화
+        for(int i=0; i<w; i++){
+            bridge.offer(0);
+        }
+        while(!bridge.isEmpty()){
+            time++;
+            //다리에서 맨 앞에 있는 것 하나 빼기
+            nowWeight -= bridge.poll();
+            //다음 트럭이 있다면
+            if(!trucks.isEmpty()){
+                //하중을 견딜 수 있는지 확인
+                //견딜 수 있다면 다음 트럭을 넣음
+                if(nowWeight+trucks.peek() <= L){
+                    nowWeight += trucks.peek();
+                    bridge.offer(trucks.poll());
+                }else bridge.offer(0);
+            }
+        }
+        System.out.println(time);
+    }
 }
