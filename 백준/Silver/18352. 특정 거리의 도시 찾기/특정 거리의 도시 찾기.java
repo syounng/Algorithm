09c8756ad6898d632d8d4368e5d4cd381c4869c3
syounng <stellaj0522@gmail.com>
2025-02-17 08:49:54 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 972 ms, Memory: 274152 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,73 +1,75 @@
-import java.util.*;
-import java.lang.Comparable;
 import java.io.*;
+import java.util.*;
+
 public class Main {
     static int N, M, K, X;
-    static ArrayList<Node>[] arr;
-    static int[] dist;
+    static int[] ans;
+    static List<List<Integer>> list;
+    static boolean[] check;
 
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st;
-      st = new StringTokenizer(br.readLine());
-      N = Integer.parseInt(st.nextToken());
-      M = Integer.parseInt(st.nextToken());
-      K = Integer.parseInt(st.nextToken());
-      X = Integer.parseInt(st.nextToken());
-      arr = new ArrayList[N+1];
-      for (int i = 0; i <= N; i++){
-          arr[i] = new ArrayList<>();
-      }
-      dist = new int[N+1];
-      Arrays.fill(dist, Integer.MAX_VALUE);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken())-1;
 
-      for (int i = 0; i < M; i++) {
-          st = new StringTokenizer(br.readLine());
-          int x = Integer.parseInt(st.nextToken());
-          int y = Integer.parseInt(st.nextToken());
-          arr[x].add(new Node(y, 1)); // 기본 가중치.
-      }
-      BFS(X);
+        list = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            list.add(new ArrayList<>());
+        }
 
-      boolean ck = false;
-      for (int i = 1; i <= N; i++) {
-          if (dist[i] == K) {
-              System.out.println(i);
-              ck = true;
-          }
-      }
-      if (!ck)
-          System.out.println(-1);
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            list.get(a).add(b);
+            //list.get(b).add(a);
+        }
+
+        check = new boolean[N];
+        check[X] = true;
+
+        ans = new int[N];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[X] = 0;
+
+        bfs(X);
+
+        boolean found = false;
+        for(int i=0; i<N; i++){
+            if(ans[i]==K) {
+                System.out.println(i+1);
+                found=true;
+            }
+        }
+        if(!found) System.out.println(-1);
 
     }
-    public static void BFS(int v) {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(v, 0));
-        dist[v] = 0;
-        boolean[] visited = new boolean[N+1];
+    static void bfs(int x){
+        Queue<City> q = new LinkedList<>();
+        q.add(new City(x, 0));
 
-        while(!q.isEmpty()) {
-            Node now = q.poll();
-            if (visited[now.index])
-                continue;
-            visited[now.index] = true;
-            for (Node next: arr[now.index]) {
-                if (!visited[next.index] && dist[next.index] > (next.dist + dist[now.index])) {
-                    dist[next.index] = dist[now.index] + next.dist;
-                    q.add(new Node(next.index, dist[next.index]));
+        while(!q.isEmpty()){
+            City now = q.poll();
+            List<Integer> cities = list.get(now.num);
+            for(int i=0; i<cities.size(); i++){
+                int next = cities.get(i);
+                if(!check[next]){
+                    ans[next] = now.len+1;
+                    check[next] = true;
+                    q.add(new City(next, now.len+1));
                 }
             }
         }
     }
 }
+class City{
+    int num, len;
+    City(int num, int len){
+        this.num = num;
+        this.len = len;
+    }
 
-class Node implements Comparable<Node> {
-    int index, dist;
-    Node (int index, int dist) {
-        this.index = index;
-        this.dist = dist;
-    }
-    @Override public int compareTo(Node n) {
-        return dist - n.dist;
-    }
 }
