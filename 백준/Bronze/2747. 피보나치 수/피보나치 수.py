4d9743056bc02a3dd6863887548dfd0594d85178
정소영 <stellaj0522@gmail.com>
2025-03-19 23:02:36 +0900

    [Bronze II] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,17 +1,16 @@
-# n이 주어졌을 때 n번째 피보나치 수를 출력하여라
+N = int(input())
 
-n = int(input())
+def fib(n):
+    if n==0:
+        return [0]
+    elif n==1:
+        return [0, 1]
+    elif n==2:
+        return [0, 1, 1]
+    else:
+        tmplist = fib(n-1)
+        tmplist.append(tmplist[-1]+tmplist[-2])
+        return tmplist
 
-# 0, 1, 2번째값을 초기값으로 정의
-list = []
-if n==0:
-    list = [0]
-elif n==1:
-    list = [0, 1]
-elif n==2:
-    list = [0, 1, 1]
-else:
-    list = [0, 1, 1]
-    for i in range(3, n+1):
-        list.append(list[i-1]+list[i-2])
-print(list[n])
+list = fib(N)
+print(list[N])
