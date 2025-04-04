851414b0b80637bffacff6e2dfea92565ea3eb6c
정소영 <stellaj0522@gmail.com>
2025-04-04 23:49:07 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 19036 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -15,12 +15,20 @@ public class Main {
         len1 = s1.length();
         len2 = s2.length();
 
-        a = s1.toCharArray();
-        b = s2.toCharArray();
+        // 인덱스 1부터 사용하기 위해 크기를 len+1로 설정
+        a = new char[len1 + 1];
+        b = new char[len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            a[i] = s1.charAt(i - 1); // 1부터 시작하도록 저장
+        }
+        for (int i = 1; i <= len2; i++) {
+            b[i] = s2.charAt(i - 1); // 1부터 시작하도록 저장
+        }
 
         d = new int[len1 + 1][len2 + 1];
 
-        // 메모이제이션 배열을 -1로 초기화 (방문 여부 체크)
+        // 메모이제이션 배열을 -1로 초기화
         for (int[] row : d) {
             Arrays.fill(row, -1);
         }
@@ -28,19 +36,16 @@ public class Main {
         System.out.println(lcs(len1, len2)); // 전체 문자열을 비교
     }
 
-    static int lcs(int i, int j) { // a[0..i-1], b[0..j-1]에서의 LCS 길이
-        if (i == 0 || j == 0) // 더 이상 비교할 문자가 없으면 0 반환
+    static int lcs(int i, int j) { // a[1..i], b[1..j]에서의 LCS 길이
+        if(a[i]==0 || b[j]==0)
             return 0;
-
-        if (d[i][j] != -1) // 이미 계산한 값이면 반환
+        if(d[i][j] != -1){
             return d[i][j];
-
-        if (a[i - 1] == b[j - 1]) { // 문자가 같을 경우
-            d[i][j] = lcs(i - 1, j - 1) + 1;
-        } else { // 문자가 다를 경우
-            d[i][j] = Math.max(lcs(i - 1, j), lcs(i, j - 1));
         }
-
-        return d[i][j];
+        if(a[i] == b[j]){
+            return d[i][j] = lcs(i-1, j-1) + 1;
+        }else{
+            return d[i][j] = Math.max(lcs(i-1, j), lcs(i, j-1));
+        }
     }
 }
